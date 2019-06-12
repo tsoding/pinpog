cb66880b2f9351c1471dd97e5840eadf4402a868
rexim <reximkut@gmail.com>
2019-06-12 23:46:19 +0700

    Hello

--- pinpog.asm
+++ pinpog.asm
@@ -1,4 +1,17 @@
-loop:
-    jmp loop
+    mov ah, 0x0e
+
+    mov al, 'H'
+    int 0x10
+    mov al, 'e'
+    int 0x10
+    mov al, 'l'
+    int 0x10
+    mov al, 'l'
+    int 0x10
+    mov al, 'o'
+    int 0x10
+
+    jmp $
+
     times 510 - ($-$$) db 0
     dw 0xaa55
