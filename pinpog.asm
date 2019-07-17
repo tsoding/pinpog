0e13121b2ad34e1c7348a38fbe0dbad095d48613
rexim <reximkut@gmail.com>
2019-07-18 03:12:10 +0700

    Remove TODO(#19)

--- pinpog.asm
+++ pinpog.asm
@@ -285,7 +285,6 @@ fill_rect:
 
 ;; TODO(#18): Game does not keep track of the score
 ;;   Every bar hit should give you points
-;; TODO(#19): Game does not get harder over time
 ;; TODO(#46): initial_game_state should probably initialized using istruc mechanism
 initial_game_state:
 _state: dw running_state
